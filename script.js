9e4c4fec5d123891bc025c230a7987113dc9531f
Bhum1k <daboom2110@gmail.com>
2023-08-03 18:14:22 +1200

    hello

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
 // script.js
 fetch('/.netlify/functions/getCurrentMap')
-  .then(response => response.text()) // Use response.text() to get plain text
+  .then(response => response.json())
   .then(data => {
-    const genieContainer = document.querySelector('div#__genieContainer');
-    if (genieContainer) {
-      // Access the shadowRoot of the genieContainer
-      const shadowRoot = genieContainer.shadowRoot || genieContainer.attachShadow({ mode: 'open' });
-
-      // Create a new text node with the map data
-      const textNode = document.createTextNode(data || 'Data not available');
-
-      // Clear the existing content and append the new text node
-      shadowRoot.innerHTML = '';
-      shadowRoot.appendChild(textNode);
-    }
+    const currentMapElement = document.getElementById('currentMap');
+    currentMapElement.textContent = data.currentMap || 'Data not available';
   })
   .catch(error => {
-    const genieContainer = document.querySelector('div#__genieContainer');
-    if (genieContainer) {
-      // Access the shadowRoot of the genieContainer
-      const shadowRoot = genieContainer.shadowRoot || genieContainer.attachShadow({ mode: 'open' });
-
-      // Create a new text node with the error message
-      const errorTextNode = document.createTextNode('Failed to fetch data');
-
-      // Clear the existing content and append the new text node
-      shadowRoot.innerHTML = '';
-      shadowRoot.appendChild(errorTextNode);
-    }
+    const currentMapElement = document.getElementById('currentMap');
+    currentMapElement.textContent = 'Failed to fetch data';
   });
