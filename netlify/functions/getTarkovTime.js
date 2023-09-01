d25fcce7abf6fc95b2acf58e0e8d0e55d58a6a6f
Bhum1k <daboom2110@gmail.com>
2023-09-02 02:57:45 +1200

    test

--- netlify/functions/getTarkovTime.js
+++ netlify/functions/getTarkovTime.js
@@ -4,31 +4,16 @@ exports.handler = async (event, context) => {
   try {
     const response = await axios.get('https://tarkov-time.adam.id.au/api');
     const data = response.data;
+    const left = data["left"][0];
+    const right = data["right"][0];
+    const output = left.slice(0,-3);
 
-    // Extract the time strings (HH:mm:ss) from the API response
-    const leftTimeStr = data.left.split(' ')[1];
-    const rightTimeStr = data.right.split(' ')[1];
 
-    // Split the time strings into hours, minutes, and seconds
-    const leftTimeParts = leftTimeStr.split(':');
-    const rightTimeParts = rightTimeStr.split(':');
 
-    // Convert the time parts to integers
-    const leftHours = parseInt(leftTimeParts[0]);
-    const leftMinutes = parseInt(leftTimeParts[1]);
-    const rightHours = parseInt(rightTimeParts[0]);
-    const rightMinutes = parseInt(rightTimeParts[1]);
-
-    // Format the times in 12-hour format (h:mm a)
-    const formattedLeftTime = `${leftHours % 12}:${leftMinutes.toString().padStart(2, '0')} ${leftHours >= 12 ? 'PM' : 'AM'}`;
-    const formattedRightTime = `${rightHours % 12}:${rightMinutes.toString().padStart(2, '0')} ${rightHours >= 12 ? 'PM' : 'AM'}`;
-
-    // Combine the formatted times into a single string
-    const result = `${formattedLeftTime}/${formattedRightTime}`;
 
     return {
       statusCode: 200,
-      body: result,
+      body: output,
       headers: {
         'Content-Type': 'text/plain',
       },
@@ -36,10 +21,11 @@ exports.handler = async (event, context) => {
   } catch (error) {
     return {
       statusCode: 500,
-      body: 'Failed to fetch and format data',
+      body: 'Failed to fetch data',
       headers: {
         'Content-Type': 'text/plain',
       },
     };
   }
 };
+
