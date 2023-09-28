3434f3212ff5c764234f4ae9e271f4b2cd32c02c
Bhum1k <daboom2110@gmail.com>
2023-09-28 20:08:04 +1300

    Update getCurrentMap.js

--- netlify/functions/getCurrentMap.js
+++ netlify/functions/getCurrentMap.js
@@ -1,45 +1,57 @@
-const { google } = require('googleapis');
-const sheets = google.sheets('v4');
+const axios = require('axios');
 
-// Load the credentials from the JSON key file
-const credentials = require('goons.json');
+exports.handler = async (event, context) => {
+  try {
+    const response = await axios.get('https://www.tarkovpal.com/api');
+    const response2 = await axios.get('https://decapi.me/misc/time?timezone=America/Chicago&format=F%20j,%20o,%20g:i%20a')
+    const data = response.data;
+    const central = response2.data;
+    const currentMap = data["Current Map"][0];
+    const apiDate = data["Time"][0];
+    
+    const minutes = calculateTimeDifferenceInMinutes(apiDate, central);
+    
+    const output = currentMap.concat(' reported ', minutes, ' minutes ago');
 
-const auth = new google.auth.JWT({
-  email: credentials.client_email,
-  key: credentials.private_key,
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
+    // Set the environment variable
+    process.env.CURRENT_MAP = currentMap;
 
-// The ID of the Google Sheet you want to access
-const spreadsheetId = '2PACX-1vRwLysnh2Tf7h2yHBc_bpZLQh6DiFZtDqyhHLYP022xolQUPUHkSModV31E5Y7cLh_8LZGexpXy2VuH';
-
-auth.authorize(function (err) {
-  if (err) {
-    console.error('Authorization Error:', err);
-    return;
+    return {
+      statusCode: 200,
+      body: output,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Failed to fetch data',
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    };
   }
+};
 
-  sheets.spreadsheets.values.get(
-    {
-      auth: auth,
-      spreadsheetId: spreadsheetId,
-      range: 'Sheet1!A2:B2', // Specify the range you want to extract data from
-    },
-    function (err, response) {
-      if (err) {
-        console.error('The API returned an error:', err);
-        return;
-      }
+// Function to calculate the time difference in minutes between two date strings
+function calculateTimeDifferenceInMinutes(dateString1, dateString2) {
+  // Parse the date strings into Date objects
+  const date1 = new Date(dateString1);
+  const date2 = new Date(dateString2);
 
-      const values = response.data.values;
-      if (!values || values.length === 0) {
-        console.log('No data found.');
-      } else {
-        console.log('Data:');
-        for (const row of values) {
-          console.log(row.join('\t'));
-        }
-      }
-    }
-  );
-});
+  // Calculate the time difference in milliseconds
+  const timeDifferenceMs = date2 - date1;
+
+  // Convert milliseconds to minutes
+  const timeDifferenceMinutes = timeDifferenceMs / (1000 * 60);
+
+  return Math.abs(timeDifferenceMinutes); // Use Math.abs to ensure a positive result
+}
+
+// Example usage
+const dateString1 = "September 20, 2023, 1:26 am";
+const dateString2 = "September 21, 2023, 2:45 pm";
+
+const timeDifference = calculateTimeDifferenceInMinutes(dateString1, dateString2);
+console.log(`Time difference in minutes: ${timeDifference}`);
