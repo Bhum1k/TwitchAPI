47cc780ababc1ac4d93122456f6ea3558e2bc0f6
Bhum1k <daboom2110@gmail.com>
2023-09-28 20:03:21 +1300

    ab

--- netlify/functions/getCurrentMap.js
+++ netlify/functions/getCurrentMap.js
@@ -1,22 +1,45 @@
-const axios = require('axios');
+const { google } = require('googleapis');
+const sheets = google.sheets('v4');
 
-// Replace with the URL of the published spreadsheet
-const spreadsheetUrl =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vRwLysnh2Tf7h2yHBc_bpZLQh6DiFZtDqyhHLYP022xolQUPUHkSModV31E5Y7cLh_8LZGexpXy2VuH/pubhtml';
+// Load the credentials from the JSON key file
+const credentials = require('./goons.json');
 
-// Example range to fetch data from
-const range = 'Sheet1!A2:B2';
+const auth = new google.auth.JWT({
+  email: credentials.client_email,
+  key: credentials.private_key,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
 
-// Construct the API endpoint
-const apiUrl = `${spreadsheetUrl}?gid=0&single=true&range=${range}`;
+// The ID of the Google Sheet you want to access
+const spreadsheetId = '2PACX-1vRwLysnh2Tf7h2yHBc_bpZLQh6DiFZtDqyhHLYP022xolQUPUHkSModV31E5Y7cLh_8LZGexpXy2VuH';
 
-// Make a GET request to fetch the data
-axios
-  .get(apiUrl)
-  .then((response) => {
-    const data = response.data;
-    console.log('Data:', data);
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  });
+auth.authorize(function (err) {
+  if (err) {
+    console.error('Authorization Error:', err);
+    return;
+  }
+
+  sheets.spreadsheets.values.get(
+    {
+      auth: auth,
+      spreadsheetId: spreadsheetId,
+      range: 'Sheet1!A2:B2', // Specify the range you want to extract data from
+    },
+    function (err, response) {
+      if (err) {
+        console.error('The API returned an error:', err);
+        return;
+      }
+
+      const values = response.data.values;
+      if (!values || values.length === 0) {
+        console.log('No data found.');
+      } else {
+        console.log('Data:');
+        for (const row of values) {
+          console.log(row.join('\t'));
+        }
+      }
+    }
+  );
+});
