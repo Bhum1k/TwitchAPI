0c7099f7aa38ccc94d4ff431c4fec42c0d7c50b9
Bhum1k <daboom2110@gmail.com>
2024-01-03 01:24:27 +1300

    Update quest.js

--- netlify/functions/quest.js
+++ netlify/functions/quest.js
@@ -13,15 +13,21 @@ exports.handler = async function (event, context) {
         
         const yourProcessedData = queryTerm;
 
+            let output = "";
     
             marketUrl = `https://api.tarkov-market.app/api/v1/nightbot?x-api-key=6dJ67FuraCJjcxjd&q=${queryTerm}`;
-            priceResponse = await axios.get(marketUrl);
-            marketData = priceResponse.data;
-            let output = marketData;
-            if (marketData.includes("₽")) {
-                let array = marketData.split(" ");
-                concatenated = array.slice(0, -1).join(" ");
-                output = concatenated;
+            try {
+                priceResponse = await axios.get(marketUrl);
+                marketData = priceResponse.data;
+                output = marketData;
+                if (marketData.includes("₽")) {
+                    let array = marketData.split(" ");
+                    concatenated = array.slice(0, -1).join(" ");
+                    output = concatenated;
+            } catch (error) {
+                output = "ERROR"
+            }
+
         }
         
 
@@ -32,41 +38,14 @@ exports.handler = async function (event, context) {
             headers: {
             'Content-Type': 'application/json',
         }
+    }; catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Error", error: error.message }),
+        };
     };
-} catch (error) {
-    return {
-        statusCode: 500,
-        body: JSON.stringify({ message: "Error", error: error.message }),
-    };
-};
-
-function searchAmmo(query) {
-    // Convert the query to lowercase for case-insensitive matching
-    const lowercaseQuery = query.toLowerCase();
-
-    // Split the query into individual terms
-    const queryTerms = lowercaseQuery.split(/\s+/);
-
-    // Initialize variables to store the best match and its index
-    let bestMatch = '';
-    let bestMatchIndex = -1;
-
-    // Loop through the array to find the best match
-    for (let i = 0; i < searchterms.length; i++) {
-        const currentTerm = searchterms[i].toLowerCase();
-
-        // Check if all query terms are present in the current term
-        const allTermsPresent = queryTerms.every(term => currentTerm.includes(term));
-
-        if (allTermsPresent) {
-            // Update the best match if the current term is a better match
-            if (currentTerm.length > bestMatch.length) {
-                bestMatch = currentTerm;
-                bestMatchIndex = i;
-            }
-        }
-    }
 
     // Return the index of the best match
     return bestMatchIndex;
+    }
 }
