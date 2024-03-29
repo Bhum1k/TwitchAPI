6a5f37a5a2d2c6e96899382eaa8be7c01cd1e470
Bhum1k <daboom2110@gmail.com>
2024-03-29 16:02:23 +1300

    Update bday.js

--- netlify/functions/bday.js
+++ netlify/functions/bday.js
@@ -1,99 +1,33 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 exports.handler = async (event, context) => {
-  try {
-    const response2 = await axios.get('https://decapi.me/misc/time?timezone=America/New_York&format=n/j/o%20G:i:s')
-    const central = response2.data;
-    const data = await scraping();
-    const currentMap = data[2];
-    const apiDate = data[1];
-    const minutes = calculateTimeDifferenceInMinutes(apiDate, central);
+    try {
+        const response = await axios.get('https://tarkov-time.adam.id.au/api');
+        const data = response.data;
+        const left = data.left;
+        const right = data.right;
+        const left_slice = left.slice(0, -3);
+        const right_slice = right.slice(0, -3);
+        const output = left_slice.concat('/', right_slice)
 
-    const response = await axios.get('https://www.tarkovpal.com/api');
-    const response3 = await axios.get('https://decapi.me/misc/time?timezone=America/Chicago&format=F%20j,%20o,%20g:i%20a')
-    const data2 = response.data;
-    const centralTime = response3.data;
-    const currentMapTP = data2["Current Map"][0];
-    const apiDateTP = data2["Time"][0];
 
-    const minutesTP = calculateTimeDifferenceInMinutesTP(apiDateTP, centralTime);
 
-    let output = "";
-    if (minutesTP > minutes) {
-      output = currentMap.concat(' reported ', minutes, ' minutes ago');
+
+        return {
+            statusCode: 200,
+            body: output,
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Failed to fetch data',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+        };
     }
-    else {
-      output = currentMapTP.concat(' reported ', minutesTP, ' minutes ago');
-    }
-    console.log(output);
-    // Set the environment variable
-    process.env.CURRENT_MAP = currentMap;
-
-    return {
-      statusCode: 200,
-      body: output,
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: 'Failed to fetch data',
-      headers: {
-        'Content-Type': 'text/plain',
-      },
-    };
-  }
 };
 
-// Function to calculate the time difference in minutes between two date strings
-function calculateTimeDifferenceInMinutes(dateString1, dateString2) {
-  // Parse the date strings into Date objects
-  const date1 = new Date(dateString1);
-  const date2 = new Date(dateString2);
-
-  // Calculate the time difference in milliseconds
-  const timeDifferenceMs = date2 - date1;
-
-  // Convert milliseconds to minutes
-  const timeDifferenceMinutes = Math.floor(timeDifferenceMs / (1000 * 60));
-  return Math.abs(timeDifferenceMinutes); // Use Math.abs to ensure a positive result
-}
-
-function split(str, index) {
-  const result = [str.slice(0, index), str.slice(index)];
-
-  return result;
-}
-
-
-async function scraping() {
-  const resposne = await axios.request({
-    method: "GET",
-    url: "https://docs.google.com/spreadsheets/u/2/d/e/2PACX-1vR-wIQI351UH85ILq5KiCLMMrl0uHRmjDinBCt6nXGg5exeuCxQUf8DTLJkwn7Ckr8-HmLyEIoapBE5/pubhtml/sheet?headers=false&gid=1420050773"
-  });
-
-  const $ = cheerio.load(resposne.data);
-
-  const element = $(".s0").text();
-  const [first, second] = split(element, 23);
-  const dataArr = second.match(/(\d{1,2}\/\d{1,2}\/\d{4}\s\d{1,2}:\d{2}:\d{2})([A-Za-z]+)/);
-  return dataArr
-}
-
-// Function to calculate the time difference in minutes between two date strings
-function calculateTimeDifferenceInMinutesTP(dateString1, dateString2) {
-  // Parse the date strings into Date objects
-  const date1 = new Date(dateString1);
-  const date2 = new Date(dateString2);
-
-  // Calculate the time difference in milliseconds
-  const timeDifferenceMs = date2 - date1;
-
-  // Convert milliseconds to minutes
-  const timeDifferenceMinutes = timeDifferenceMs / (1000 * 60);
-
-  return Math.abs(timeDifferenceMinutes); // Use Math.abs to ensure a positive result
-}
