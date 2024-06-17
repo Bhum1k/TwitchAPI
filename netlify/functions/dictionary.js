fd900bcc644535397012adab20128b7ca123b080
Bhum1k <daboom2110@gmail.com>
2024-06-18 10:40:22 +1200

    fix

--- netlify/functions/dictionary.js
+++ netlify/functions/dictionary.js
@@ -2,16 +2,25 @@ const fs = require('fs');
 const axios = require('axios');
 
 async function returnOutput(query) {
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${query}`;
+    const url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=8478128f-c2a2-4737-b81d-e0146f5392d9`;
 
     try {
         const response = await axios.get(url);
         const data = response.data;
 
-        return (`${data[0].word} (${data[0].meanings[0].partOfSpeech}) - ${data[0].meanings[0].definitions[0].definition}`);
+        return (`${data[0].meta.id} (${data[0].fl}) - ${data[0].shortdef[0]}`);
     } catch (error) {
-        console.error('There was an error fetching the dictionary data:', error);
-        return 'Word not found!!!';
+        const url2 = `https://www.dictionaryapi.com/api/v3/references/medical/json/${query}?key=d137241f-f3eb-4abd-9d30-28a6c627c414`;
+
+        try {
+            const response = await axios.get(url2);
+            const data = response.data;
+
+            return (`${data[0].meta.id} (${data[0].fl}) - ${data[0].shortdef[0]}`);
+        } catch (error) {
+            console.error('There was an error fetching the dictionary data:', error);
+            return 'Word not found!!!';
+        }
     }
 }
 
